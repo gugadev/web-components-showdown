0f1f02dc67083a2aa6064b9618217420c2376807
Gus Garzaki <gusgarzaki@gmail.com>
2019-05-13 17:54:16 -0500

    ignore terser for now

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import { resolve } from 'path'
 import node from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import postcss from 'rollup-plugin-postcss'
-import { terser } from 'rollup-plugin-terser'
+// import { terser } from 'rollup-plugin-terser'
 
-const ELEMENTS_PATH = resolve(__dirname, '..', 'src', 'components')
-const DIST_PATH = resolve(paths.dist, `${name}.mjs`)
+const ELEMENTS_PATH = resolve(__dirname, 'src')
+const DIST_PATH = resolve(__dirname, 'dist')
 
 const plugins = [
   node({ module: true }),
@@ -14,11 +14,11 @@ const plugins = [
     modules: false,
     inject: false
   }),
-  terser()
+  // terser()
 ]
 
 const buildConfig = (name) => ({
-  input: resolve(ELEMENTS_PATH, name, `${name}.module.ts`),
+  input: resolve(ELEMENTS_PATH, name, `${name}.component.ts`),
   output: {
     file: resolve(DIST_PATH, name, `${name}.mjs`),
     format: 'esm'
